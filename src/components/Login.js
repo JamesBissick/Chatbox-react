b8ba3cdfdc7a136e5e35baa325892c70739659ba
James Bissick <bissickj@gmail.com>
2021-04-22 20:52:11 +0200

    Feat: Adding Routes logic

---
+++ src/components/Login.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import { Redirect } from "react-router-dom";
+
+
+class Login extends Component {
+  state = {
+    username: '',
+    goToChat: false
+  }
+
+  handleChange = event => {
+    const username = event.target.value;
+    this.setState({ username });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ goToChat: true });
+  }
+
+  render() {
+    if (this.state.goToChat) {
+      return <Redirect to={`/Users/${this.state.username}`} />
+    }
+
+    return (
+      <div className='login-box'>
+        <form className='login' onSubmit={this.handleSubmit}>
+          <input type="text" placeholder='Username' value={this.state.username} onChange={this.handleChange} required />
+          <button type="submit">Go</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Login from "./components/Login.js";
+import NotFound from "./components/NotFound.js";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Root = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={ Login } />
+      <Route path='/Users/:user' component={ App } />
+      <Route component={ NotFound } />
+    </Switch>
+  </BrowserRouter>
+)
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
